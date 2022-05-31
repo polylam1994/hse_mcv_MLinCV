796180f7651a9549653f4cc12ebceb163b340239
TheOldManAndTheSea <59043071+polylam1994@users.noreply.github.com>
2022-05-31 16:38:38 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,30 @@
+# DBFace
+
+## Introduction
+
+This is a HSE MCV week2 task 2 - Setup CI pipeline
+
+## Installation
+
+---Install virtualenv---
+python -m pip install --user virtualenv
+
+---Change to project directory---
+cd paste/your/directory/here
+
+---Create Python virtual enviroment under the directory---
+python -m venv env
+
+---Activate Python virtual environment---
+.\env\Scripts\activate
+
+---Install all the dependencies---
+pip install -r requirements.txt
+pip install -r requirements_dev.txt
+
+
+---Activate Python virtual environment if you havn't---
+.\env\Scripts\activate
+
+---Run Pytest---
+pytest
---
+++ test/test_demo.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 31 14:48:55 2022
+
+@author: 6495
+"""
+
+
+import os.path as osp
+import numpy as np
+import pytest
+from hse_mcv_dbface import demo
+
+
+def prepare_image_names(list_of_short_names):
+    path_to_images = osp.abspath(osp.dirname(__file__)) + "/test_images/"
+    images_for_testing = [path_to_images + image for image in list_of_short_names]
+    return images_for_testing
+
+
+# A regression test using a single image that checks results are always the same
+@pytest.mark.parametrize("image_name", prepare_image_names(["test.jpg"]))
+def test_regression(image_name):
+    first_result = demo.image_demo(image_name, show_image=False)
+    second_result = demo.image_demo(image_name, show_image=False)
+    assert np.array_equal(first_result,second_result)
+
+
+# A “no error” test with big image, small image
+@pytest.mark.parametrize("image_name", prepare_image_names(["small.jpg","big.jpg"]))
+def test_for_errors_on_images_of_different_sizes(image_name):
+    demo.image_demo(image_name, show_image=False)
